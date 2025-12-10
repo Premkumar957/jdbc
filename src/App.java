d89507961c0e0567bfa70405b292af3262eb2743
Premkumar <premkumar05604@gmail.com>
2025-12-10 15:30:40 +0530

    Working on statement, preparedStatemet and Callable Statement

--- src/App.java
+++ src/App.java
@@ -1,23 +1,48 @@
-import java.sql.*;
+//Step 1: Import the package
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String url = "jdbc:mysql://127.0.0.1:3306/practice";
         String user = "root";
         String password = "root";
 
-        String query = "SELECT id, name, age FROM students";
+          
+        //Step 2: Load and register the Driver (optional)
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        try (Connection con = DriverManager.getConnection(url, user, password)) {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getInt("age"));
-            }
-        } catch (SQLException e) {
-            System.out.println("It not work");
-            e.printStackTrace();
+        //Step 3: Establish Connection
+        Connection con = DriverManager.getConnection(url,user, password);
+
+        //Step 4: Create a Statement || PreparedStatement || CallableStatement
+        String city = "Chennai";
+        CallableStatement cs = con.prepareCall("{call getSameCityCustomers(?)}");
+        cs.setString(1, city);
+
+        
+
+        //Step 5: Execute the statment/Sql Query
+        ResultSet rs = cs.executeQuery();
+
+        
+        
+    
+       
+
+        // // Step 6: Process the results
+        while (rs.next()) {
+            System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getString("city") + " "  + rs.getInt("pincode"));
         }
+
+        //Step 7: Close the connection
+        rs.close();
+        cs.close();
+        con.close();
+
+
 
     }
 }
